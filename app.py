d363f4f12bd1a0b1c05cd36ac59d6ea2d013ecdd
Rogerson Nazário <rrnazario@gmail.com>
2018-06-11 11:11:03 -0300

    "GET /webhook HTTP/1.1" 500

--- app.py
+++ app.py
@@ -21,18 +21,7 @@ app = Flask(__name__)
 
 @app.route('/webhook', methods=['GET', 'POST'])
 def webhook():
-    req = request.get_json(silent=True, force=True)
-
-    print("Request: " + req)
-    #print(json.dumps(req, indent=4))
-
-    res = processRequest(req)
-
-    res = json.dumps(res, indent=4)
-    # print(res)
-    r = make_response(res)
-    r.headers['Content-Type'] = 'application/json'
-    return r
+    return "Webhook"
 
 
 def processRequest(req):
